12d0f4fd9f6c1a84ce05565152b9419687cb002d
Idrissa <idrisabkar2018@gmail.com>
2023-12-31 19:45:43 +0300

    Registered all models in admin.py: Enabled Django admin interface for all application models

--- order/models.py
+++ order/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from product.models import Product
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Order(models.Model):
+    def generate_order_code():
+        import uuid
+        return "BDI-" + str(uuid.uuid4()).upper().split("-")[-1][:12]
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    products = models.ManyToManyField(Product, through='OrderProduct')
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=20, default='pending')
+    total = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
+    address = models.CharField(max_length=200)
+    code = models.CharField(max_length=200, default=generate_order_code)
+    
+    
+    
+
+    def __str__(self):
+        return self.product.name
