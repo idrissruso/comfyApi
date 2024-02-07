c128eb077f299715e385ee481dead153a01f3734
Idrissa <idrisabkar2018@gmail.com>
2024-02-07 22:36:20 +0300

    Created CustomUserCreationForm in forms.py: Extended UserCreationForm to include email validation and made email field required

--- authentication/views.py
+++ authentication/views.py
@@ -1,5 +1,11 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.decorators import api_view
+from .customForm import CustomUserCreationForm
+from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
+
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     @classmethod
@@ -14,3 +20,12 @@ class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
     
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
+    
+class RegisterView(APIView):
+    @api_view(['POST'])
+    def register(request):
+        serializer = CustomUserCreationForm(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message': 'User created successfully'}, status=status.HTTP_200_OK)
+        return Response({'message': serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
